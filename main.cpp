a007d6c42508fd4866fcaf75ec3a8e69029a7558
VOID001 <zhangjianqiu13@gmail.com>
2014-12-30 17:01:41 +0800

    Create an Blank main File

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include"comm.h"
+#include"structs.h"
+#include""
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    mainWindow w;
-    w.show();
 
-    return a.exec();
 }
