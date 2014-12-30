9b0e4cbe4e642d7afb6bacc54ad0604e8702c27c
VOID001 <zhangjianqiu13@gmail.com>
2014-12-30 16:59:10 +0800

    Add definition of classHuffmanTree

--- classHuffmanTree.h
+++ classHuffmanTree.h
@@ -1,4 +1,33 @@
 #ifndef CLASSHUFFMANTREE_H
 #define CLASSHUFFMANTREE_H
+#include"comm.h"
+#include"struct.h"
+
+class HuffmanTree{
+    private:
+        void createHuffmanTree();
+        void countFreq();
+        void generateCodingTable();
+        void pr_encoding();
+        void pr_decoding();
+        int root;
+        vector<StaticHuffmanNode> HuffmanT;
+        string codingTable[300];
+        string fileStr;
+    public:
+        void encoding();
+        void decoding();
+        void open();
+        HuffmanTree();
+        ~HuffmanTree();
+        HuffmanTree(const HuffmanTree& T);
+
+        //******For Debug********
+        //Delete when the project Done
+#ifdef DEBUG
+        void debug();
+#endif
+        //******END FOR DEBUG****
+};
 
 #endif // CLASSHUFFMANTREE_H
